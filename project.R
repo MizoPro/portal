6ac0a2c61a3761796e565dcae9bca82188fb1ee1
Hamza Belhadj <hamza.randy@gmail.com>
2018-12-11 14:55:51 +0100

    R Project -FINAL- done

---
+++ project.R
@@ -0,0 +1,51 @@
+# Packages
+
+library(haven)
+library(dplyr)
+library(reshape2)
+library(ggplot2)
+
+d <- read_sav("merged_r5_data.sav")
+
+INVALID  <- c(-1, 9, 988, 7) # -1: missing, 9: don't know, 988: refused to answer, 7: No experience with schools...
+
+VA <- c("Expensive", "Tools", "Poor.Teaching", "Absent.Teachers", "Overcrowded.Classrooms", "Poor.Facilities")
+VB <- c("Never", "Once or twice", "A few times", "Often")
+
+# 7 Questions
+d_2 <- select(d, URBRUR, Q68A, Q68B, Q68C, Q68D, Q68E, Q68F)
+d_2 <- d_2[!(d$URBRUR == 3 | d$Q68A %in% INVALID | d$Q68B %in% INVALID | d$Q68C %in% INVALID | d$Q68D %in% INVALID | d$Q68E %in% INVALID | d$Q68F %in% INVALID), ]
+View(head(d_2))
+
+# Les variables
+Expensive <- summary(factor(d_2$Q68A))
+Tools <- summary(factor(d_2$Q68B))
+Poor.Teaching <- summary(factor(d_2$Q68C))
+Absent.Teachers <- summary(factor(d_2$Q68D))
+Overcrowded.Classrooms <- summary(factor(d_2$Q68E)) 
+Poor.Facilities <- summary(factor(d_2$Q68F))
+
+mat <- cbind(Expensive, Tools, Poor.Teaching, Absent.Teachers, Overcrowded.Classrooms, Poor.Facilities)
+
+data <- as.data.frame(mat)
+
+
+t1 = as.data.frame(t(prop.table(as.matrix(t(data)), 1) * 100))
+t1_reshaped <- data.frame( "",
+                           independent.variable = c(t1$Expensive, t1$Tools, t1$Poor.Teaching, t1$Absent.Teachers, t1$Overcrowded.Classrooms, t1$Poor.Facilities),
+                           Legend = rep(VB, times=6),
+                           Variable.name = rep(VA, each = nrow(t1)))
+# PIE
+ggplot(t1_reshaped,aes("",independent.variable,fill=Legend)) + 
+  geom_bar(stat = "identity", width = 1) + 
+  theme(legend.position = "right") +
+  coord_polar(theta = "y") +
+  geom_label(aes(label = paste(round(independent.variable,2),"%",sep="")), position = position_stack(vjust = 0.5))+
+  facet_wrap(~Variable.name)+labs(y = "", x = "")
+
+# BARPLOT
+URBRUR <- factor(d_2$URBRUR)
+REPONSE <- factor(d_2$Q68E)
+ggplot(d_2, aes(x=URBRUR)) +
+  geom_bar(aes(fill=REPONSE))+
+  labs(x = "URBRUR", y = "Overcrowded Classrooms") + coord_flip()
